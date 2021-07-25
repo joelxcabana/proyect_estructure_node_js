02b55e302979c7b1d4ca720a983bda110ed73f18
joelCabana <joelxcabana@gmail.com>
2021-07-25 16:04:47 -0300

    agrege configuracion para base de datos y variables de entorno

---
+++ app.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const { dbConnect } = require('./config/mongo')
+
+const app = express();
+const PORT = process.env.PORT || 3000
+
+app.use(cors());
+app.use(express.json());
+
+
+dbConnect();
+app.listen(PORT, () =>{
+    console.log('api lista por el puerto ', PORT);
+})
